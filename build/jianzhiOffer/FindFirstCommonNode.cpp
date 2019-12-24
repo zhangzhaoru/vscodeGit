a791a097396b7ad670be00ce8eb536868a5becb3
zhangzhangru <13700293285@163.com>
2019-12-24 22:27:34 +0800

    Commit Code

--- build/jianzhiOffer/FindFirstCommonNode.cpp
+++ build/jianzhiOffer/FindFirstCommonNode.cpp
@@ -6,10 +6,7 @@ using namespace std;
 struct ListNode {
     int val;
     struct ListNode *next;
-    ListNode(int x):
-            val(x),next(NULL) {
-
-            }
+    ListNode(int x):val(x),next(NULL) {}
 };
 
 ListNode* FindFirstCommonNode( ListNode* pHead1, ListNode* pHead2);
