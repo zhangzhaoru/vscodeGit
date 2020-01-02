01d8673e478b72031d0666cdc00c74176d9dff30
zhangzhangru <13700293285@163.com>
2020-01-02 20:55:51 +0800

    Commit new Code

--- build/jianzhiOffer/InversePairs.cpp
+++ build/jianzhiOffer/InversePairs.cpp
@@ -2,54 +2,61 @@
 #include<vector>
 
 using namespace std;
+int InversePairs(vector<int> data);
+long long InversePairsCore(vector<int> &data,vector<int> &copy,int start,int end);
 
-int InversePairs(vector<int> data){
-    int length = data.size();
-    if(length<0)
-        return 0;;
-    vector<int> copy;
-    for(int i = 0;i<length;i++){
-        copy.push_back(data[i]);
-    }
-    long long count = InversePairsCore(data,copy,0,length-1);
-    return count %1000000007;
-}
 
-long long InversepairsCore(vector<int> &data,vector<int> &copy,int left,int right){
-    if(left==right){
-        copy[left] = data[left];
-        return 0;
+ int InversePairs(vector<int> data) {
+       int length=data.size();
+        if(length<=0)
+            return 0;
+       //vector<int> copy=new vector<int>[length];
+       vector<int> copy;
+       for(int i=0;i<length;i++)
+           copy.push_back(data[i]);
+       long long count=InversePairsCore(data,copy,0,length-1);
+       //delete[]copy;
+       return count%1000000007;
     }
-    int mid = ((right-left)>>1)+left;
-    long long leftCount,rightCount;
-    leftCount = InversepairsCore(data,copt,left,mid);
-    rightCount = InversepairsCore(data,copt,mid+1,right);
-    int i =mid;
-    int j = right;
-    int indexcopy = right;
-    long long count;
-    while(i>=left && j>=mid+1){
-        if(data[i]>data[j]){
-            copy[indexcopy--]=data[i--];
-            count += j-mid;
-        }else{
-            copy[indexcopy--]=data[j--];
-        }
+    long long InversePairsCore(vector<int> &data,vector<int> &copy,int start,int end)
+    {
+       if(start==end)
+          {
+            copy[start]=data[start];
+            return 0;
+          }
+       int length=(end-start)/2;
+       long long left=InversePairsCore(copy,data,start,start+length);
+       long long right=InversePairsCore(copy,data,start+length+1,end); 
+        
+       int i=start+length;
+       int j=end;
+       int indexcopy=end;
+       long long count=0;
+       while(i>=start&&j>=start+length+1)
+          {
+             if(data[i]>data[j])
+                {
+                  copy[indexcopy--]=data[i--];
+                  count=count+j-start-length;          //count=count+j-(start+length+1)+1;
+                }
+             else
+                {
+                  copy[indexcopy--]=data[j--];
+                }          
+          }
+       for(;i>=start;i--)
+           copy[indexcopy--]=data[i];
+       for(;j>=start+length+1;j--)
+           copy[indexcopy--]=data[j];       
+       return left+right+count;
     }
-    for(;i>=left;i--){
-        copy[indexcopy--]=data[i];
-    }
-    for(;j>=mid+1;j--){
-        copy[indexcopy--]=data[j];
-    }
-    return count+leftcount+rightcount;
-}
 
 int main(){
     vector<int> inputdata = {1,2,3,4,5,6,7,0};
-    int res = InversePairs(data);
+    int res = InversePairs(inputdata);
     cout<<"the inverse pairs count is : "<<res<<endl;
-    sysrem("pause");
+    system("pause");
     return 0;
 }
 
